694d9980e36799623c6c90f5f1dd09f273bd8690
Robotvasya <alxkipnis@gmail.com>
2023-08-15 18:18:10 +0500

    fix structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ release = '0.1.0'
 
 extensions = []
 
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -25,4 +26,6 @@ language = 'ru'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
 html_static_path = ['_static']
+
